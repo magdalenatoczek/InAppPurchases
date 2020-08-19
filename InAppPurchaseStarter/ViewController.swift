f432aa25143cc5ec730d99cd22f77986cba057aa
Magda Toczek <magdalena.toczek@gmail.com>
2020-08-19 17:08:16 +0200

    Initial Commit

---
+++ InAppPurchaseStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InAppPurchaseStarter
+//
+//  Created by Magdalena Toczek on 19/08/2020.
+//  Copyright © 2020 Popeq Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
